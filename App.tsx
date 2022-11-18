6b99b62877194513c3c713a8bb50f95a09cd4047
Jesus Clemente <jcdeveloper92@gmail.com>
2022-11-18 14:27:01 -0400

    init porject calculator

---
+++ App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import {CalculatorScreen} from './src/screen/CalculatorScreen';
+import {SafeAreaView} from 'react-native';
+
+const App = () => {
+  return (
+    <SafeAreaView>
+      <CalculatorScreen />
+    </SafeAreaView>
+  );
+};
+
+export default App;
---
+++ src/screen/CalculatorScreen.tsx
@@ -0,0 +1,11 @@
+import {Text, View} from 'react-native';
+
+export const CalculatorScreen = () => {
+  return (
+    // eslint-disable-next-line react/react-in-jsx-scope
+    <View>
+      {/* eslint-disable-next-line react/react-in-jsx-scope */}
+      <Text>CalculatorScreen</Text>
+    </View>
+  );
+};
